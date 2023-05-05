0aa92ffd91c294b8d8fc92159ee944c133d46209
himanshuSri24 <himanshu20032002@gmail.com>
2023-05-06 01:23:50 +0530

    Coming soon page done!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react"
 import "./App.css"
-import image from "./images/background.jpg"
+import ComingSoon from "./Pages/ComingSoon";
 
 function App()
 {
   return(
-  <header>
-    <section className="wrapper">
-      <div class="top">Hifazat</div>
-      <div class="bottom" aria-hidden="true">Hifazat</div>
-    </section>
-    <h2 className="glow">Launching Soon</h2>
-  </header>
+    <>
+      <ComingSoon />
+    </>
   )
 }
 export default App;
---
+++ src/Pages/ComingSoon.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ComingSoon() {
+  return (
+    <>
+        <div className="wrapper">
+            <div className="top">Hifazat</div>
+            <div className="bottom" aria-hidden="true">Hifazat</div>
+        </div>
+        <h2 className="glow">Launching Soon</h2>
+    </>
+  )
+}
+
+export default ComingSoon
