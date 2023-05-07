0e2fef5a599f7cb0b9c7576da52313900f0fde29
rishikamaheswari <rishikamaheswari@kgpian.iitkgp.ac.in>
2023-05-08 00:44:40 +0530

    Error detection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./App.css"
 import ComingSoon from "./Pages/ComingSoon";
+import Home from "./Pages/Home/Home.js";
 
 function App()
 {
   return(
     <>
       <ComingSoon />
+      <Home />
     </>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <div>
+        <Link to='www.google.com'>Hifazat</Link>
+    </div>
+  )
+}
+
+export default Navbar
